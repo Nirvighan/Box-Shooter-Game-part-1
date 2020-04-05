e733c42969a4bb0ec7a788f668a51e841387dc75
Nirvighan <62175651+Nirvighan@users.noreply.github.com>
2020-04-05 20:38:00 +0530

    Add files via upload

---
+++ Particleblue.js
@@ -0,0 +1,24 @@
+//create the class for the blue color box
+class Particleblue {
+  //create the constructor function
+  constructor(x, y, width, height) {
+    var options = {
+      //isStatic:true
+      friction: 1.0,
+      restitution: 0.8,
+      //density: 1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+
+  //create the display function
+  display() {
+    var pos = this.body.position;
+    //ellipseMode(CENTER);
+    fill("blue");
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+};
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Box-Shooter-Game-part-1
-today I have completed the box shooter game's part 1 on visual code studio.I have made the game like angry birds game . In the next part I will join the shooter with a catapult.
+# TopplingBoxes
+Toppling Boxes
